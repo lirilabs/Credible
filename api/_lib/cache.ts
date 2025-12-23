f27d56003a2aa2de248c4952055a18b2ce75eaed
lirilabs <lirilabs@gmail.com>
2025-12-23 20:05:52 +0530

    Update cache.ts

--- api/_lib/cache.ts
+++ api/_lib/cache.ts
@@ -1,10 +1,10 @@
-const store = new Map();
+const store = new Map<string, any>();
 
-export function setCache(key, value, ttl = 30000) {
-  store.set(key, value);
+export function setCache(key: string, val: any, ttl = 30000) {
+  store.set(key, val);
   setTimeout(() => store.delete(key), ttl);
 }
 
-export function getCache(key) {
+export function getCache(key: string) {
   return store.get(key);
 }
