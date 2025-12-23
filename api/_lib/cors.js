7fb0ddeaf85fb0f533b768d27f3138022c037126
lirilabs <lirilabs@gmail.com>
2025-12-23 20:23:12 +0530

    Create cors.js

---
+++ api/_lib/cors.js
@@ -0,0 +1,11 @@
+export function allowCORS(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, x-admin-key"
+  );
+}
