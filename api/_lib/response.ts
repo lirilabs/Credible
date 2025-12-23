e11d38b0ef4ef90355045c433f2b7affe04cdd55
lirilabs <lirilabs@gmail.com>
2025-12-23 20:07:44 +0530

    Update response.ts

--- api/_lib/response.ts
+++ api/_lib/response.ts
@@ -1,20 +1,16 @@
 import { decrypt } from "./crypto";
 
-export function publicPost(p: any) {
-  return {
-    id: p.id,
-    userId: p.userId,
-    tags: p.tags,
-    ts: p.ts,
-    text: p.text,
-    image: p.image
-  };
-}
+export const publicPost = (p: any) => ({
+  id: p.id,
+  userId: p.userId,
+  tags: p.tags,
+  ts: p.ts,
+  text: p.text,
+  image: p.image
+});
 
-export function adminPost(p: any) {
-  return {
-    ...p,
-    text: decrypt(p.text),
-    image: decrypt(p.image)
-  };
-}
+export const adminPost = (p: any) => ({
+  ...p,
+  text: decrypt(p.text),
+  image: decrypt(p.image)
+});
