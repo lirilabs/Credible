6347cb14acb6a2064d722fc246bdf4bbf4c08e13
lirilabs <lirilabs@gmail.com>
2025-12-23 20:07:24 +0530

    Update admin.ts

--- api/_lib/admin.ts
+++ api/_lib/admin.ts
@@ -1,8 +1,7 @@
 import { ENV } from "./env";
 
 export function requireAdmin(req: any) {
-  const key = req.headers["x-admin-key"];
-  if (key !== ENV.ADMIN_KEY) {
+  if (req.headers["x-admin-key"] !== ENV.ADMIN_KEY) {
     throw new Error("Forbidden");
   }
 }
