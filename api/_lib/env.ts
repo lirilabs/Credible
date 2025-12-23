e2258e6f2d6cc3d15ed22f40a23186be9dbb43b6
lirilabs <lirilabs@gmail.com>
2025-12-23 20:11:37 +0530

    Update env.ts

--- api/_lib/env.ts
+++ api/_lib/env.ts
@@ -1,21 +1,3 @@
-const REQUIRED = [
-  "GITHUB_OWNER",
-  "GITHUB_REPO",
-  "GITHUB_BRANCH",
-  "GITHUB_TOKEN",
-  "SECRET_KEY",
-  "ADMIN_KEY",
-  "FCM_PROJECT_ID",
-  "FCM_CLIENT_EMAIL",
-  "FCM_PRIVATE_KEY"
-];
-
-for (const k of REQUIRED) {
-  if (!process.env[k]) {
-    throw new Error(`Missing env: ${k}`);
-  }
-}
-
 export const ENV = {
   GITHUB_OWNER: process.env.GITHUB_OWNER!,
   GITHUB_REPO: process.env.GITHUB_REPO!,
@@ -25,5 +7,7 @@ export const ENV = {
   ADMIN_KEY: process.env.ADMIN_KEY!,
   FCM_PROJECT_ID: process.env.FCM_PROJECT_ID!,
   FCM_CLIENT_EMAIL: process.env.FCM_CLIENT_EMAIL!,
-  FCM_PRIVATE_KEY: process.env.FCM_PRIVATE_KEY!.replace(/\\n/g, "\n")
+  FCM_PRIVATE_KEY: process.env.FCM_PRIVATE_KEY
+    ? process.env.FCM_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : ""
 };
