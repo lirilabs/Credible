edf957c8ea61b41ca583e4817700778b658a8867
lirilabs <lirilabs@gmail.com>
2025-12-24 20:31:21 +0530

    Update admin.js

--- api/_lib/admin.js
+++ api/_lib/admin.js
@@ -1,5 +1,3 @@
-import { ENV } from "./env.js";
-
 export function isAdmin(req) {
-  return req.headers["x-admin-key"] === ENV.ADMIN_KEY;
+  return req.headers["x-admin-key"] === process.env.ADMIN_KEY;
 }
