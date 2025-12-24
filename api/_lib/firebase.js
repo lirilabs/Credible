0e778bf438f68efa396fafdeb816d4426ce23aeb
lirilabs <lirilabs@gmail.com>
2025-12-24 12:57:23 +0530

    Update firebase.js

--- api/_lib/firebase.js
+++ api/_lib/firebase.js
@@ -1,22 +1,25 @@
 import admin from "firebase-admin";
 import { ENV } from "./env.js";
 
-if (!admin.apps.length) {
-  if (
-    !ENV.FIREBASE_PROJECT_ID ||
-    !ENV.FIREBASE_CLIENT_EMAIL ||
-    !ENV.FIREBASE_PRIVATE_KEY
-  ) {
-    throw new Error("Missing Firebase environment variables");
+export function getFirebase() {
+  if (!admin.apps.length) {
+    if (
+      !ENV.FIREBASE_PROJECT_ID ||
+      !ENV.FIREBASE_CLIENT_EMAIL ||
+      !ENV.FIREBASE_PRIVATE_KEY
+    ) {
+      console.error("Firebase env missing");
+      return null;
+    }
+
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: ENV.FIREBASE_PROJECT_ID,
+        clientEmail: ENV.FIREBASE_CLIENT_EMAIL,
+        privateKey: ENV.FIREBASE_PRIVATE_KEY,
+      }),
+    });
   }
 
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: ENV.FIREBASE_PROJECT_ID,
-      clientEmail: ENV.FIREBASE_CLIENT_EMAIL,
-      privateKey: ENV.FIREBASE_PRIVATE_KEY,
-    }),
-  });
+  return admin;
 }
-
-export default admin;
