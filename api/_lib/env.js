92efcb8198aaf31ae7d6a238651481aa590d0845
lirilabs <lirilabs@gmail.com>
2025-12-24 12:44:16 +0530

    Update env.js

--- api/_lib/env.js
+++ api/_lib/env.js
@@ -1,15 +1,22 @@
 export const ENV = {
+  /* ---------------- GitHub DB ---------------- */
   GITHUB_OWNER: process.env.GITHUB_OWNER,
   GITHUB_REPO: process.env.GITHUB_REPO,
   GITHUB_BRANCH: process.env.GITHUB_BRANCH,
   GITHUB_TOKEN: process.env.GITHUB_TOKEN,
 
+  /* ---------------- Security ---------------- */
   SECRET_KEY: process.env.SECRET_KEY,
   ADMIN_KEY: process.env.ADMIN_KEY,
 
-  FCM_PROJECT_ID: process.env.FCM_PROJECT_ID,
-  FCM_CLIENT_EMAIL: process.env.FCM_CLIENT_EMAIL,
-  FCM_PRIVATE_KEY: process.env.FCM_PRIVATE_KEY
-    ? process.env.FCM_PRIVATE_KEY.replace(/\\n/g, "\n")
-    : ""
+  /* ---------------- Firebase (FCM) ---------------- */
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : "",
+
+  /* ---------------- SMTP ---------------- */
+  SMTP_EMAIL: process.env.SMTP_EMAIL,
+  SMTP_PASSWORD: process.env.SMTP_PASSWORD,
 };
