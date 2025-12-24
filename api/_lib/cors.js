55fae6736e9d5dcd14319cdee3a69e5016cc3167
lirilabs <lirilabs@gmail.com>
2025-12-24 12:58:08 +0530

    Update cors.js

--- api/_lib/cors.js
+++ api/_lib/cors.js
@@ -1,13 +1,5 @@
 export function allowCORS(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,DELETE,OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, x-admin-key"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-admin-key");
 }
-
-
