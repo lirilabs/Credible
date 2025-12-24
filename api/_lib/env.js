416230899f1112b9285f3c711efaf646b398f0bd
lirilabs <lirilabs@gmail.com>
2025-12-24 13:04:03 +0530

    Update env.js

--- api/_lib/env.js
+++ api/_lib/env.js
@@ -7,12 +7,9 @@ export const ENV = {
   SECRET_KEY: process.env.SECRET_KEY,
   ADMIN_KEY: process.env.ADMIN_KEY,
 
-  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-  FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-  FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-    : "",
-
-  SMTP_EMAIL: process.env.SMTP_EMAIL,
-  SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+  FCM_PROJECT_ID: process.env.FCM_PROJECT_ID,
+  FCM_CLIENT_EMAIL: process.env.FCM_CLIENT_EMAIL,
+  FCM_PRIVATE_KEY: process.env.FCM_PRIVATE_KEY
+    ? process.env.FCM_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : ""
 };
